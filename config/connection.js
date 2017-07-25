86d25a25b0db8349f79dedf2faef9988358eb2e1
Whitney Wong <whitney.wong@utexas.edu>
2017-07-25 10:39:19 -0500

    Updated connection.js with JawsDB MySQL code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 //Require mysql npm module
 var mysql = require("mysql");
+var connection;
 
 //Create connection for MySQL database
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "SnowFun1",
-	database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "root",
+		password: "SnowFun1",
+		database: "burgers_db"
+	});
+};
 
 //Make connection to MySQL database
 connection.connect(function(err){
