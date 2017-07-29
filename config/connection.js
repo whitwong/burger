b19242e31a9df54eb0e6e50774b2b6975fed4895
Whitney Wong <whitney.wong@utexas.edu>
2017-07-29 14:06:29 -0500

    Added procfile

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,14 @@ var mysql = require("mysql");
 var connection;
 
 //Create connection for MySQL database
-if (process.env.JAWSDB_URL){
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.NODE_ENV === "production"){
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+		user: "wqfya0kpjpagjqas",
+		password: "xmft2vizu9mgh3rc",
+		database: "a0go03dwtqhmnfua"
+	});
 }
 else {
 	connection = mysql.createConnection({
